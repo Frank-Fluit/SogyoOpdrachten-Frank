18ecb9206ef5420374be8c7970aaec972900a513
Frank Fluit <ffluit@sogyo.nl>
2023-08-09 16:34:23 +0200

    created class Node, added several instance variable

---
+++ src/main/java/nl/sogyo/javaopdrachten/Node.java
@@ -0,0 +1,27 @@
+package nl.sogyo.javaopdrachten;
+
+import java.util.*;
+
+public class Node{
+
+
+    
+
+    
+    Boolean question;
+    String name;
+    String questionOrAnswerLine;
+
+  
+
+public Node(Boolean question, String name, String questionorAnswerLine) {
+    
+    this.question = question;
+    this.name = name;
+    this.questionOrAnswerLine = questionorAnswerLine;
+    
+}
+
+
+
+}
