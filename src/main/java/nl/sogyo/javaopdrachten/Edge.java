efa461688864d727b094fd47d42ecc3e6467b6a9
Frank Fluit <ffluit@sogyo.nl>
2023-08-09 16:33:55 +0200

    Created class Edge, might omit later

---
+++ src/main/java/nl/sogyo/javaopdrachten/Edge.java
@@ -0,0 +1,22 @@
+package nl.sogyo.javaopdrachten;
+
+import java.util.*;
+
+public class Edge{
+
+
+    //ArrayList<String> nodes;
+    //answer
+    //origin
+    //destination
+
+  
+
+//public Edge(ArrayList<String> nodes) {
+    //this.nodes = nodes;
+    
+//}
+
+
+
+}
