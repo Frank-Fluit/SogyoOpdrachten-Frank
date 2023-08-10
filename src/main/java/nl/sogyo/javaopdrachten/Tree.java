82d386fab0d6a9f7ee029263fcd3846b78581859
Frank Fluit <ffluit@sogyo.nl>
2023-08-10 12:20:38 +0200

    layout aangepast, comments verwijderd

--- src/main/java/nl/sogyo/javaopdrachten/Tree.java
+++ src/main/java/nl/sogyo/javaopdrachten/Tree.java
@@ -6,15 +6,7 @@ import java.util.*;
 
 public class Tree{
 
-
-
-
-//take the nodes out of the list
-// transform the nodelist into a list of nodeinstances
-
-
-
-
+//method1 for tree
 public ArrayList<ArrayList<String>> separateNodesAndEdges(ArrayList<String> fileLines) { 
 
     ArrayList<String> nodeLines = new ArrayList<String>();
@@ -23,9 +15,7 @@ public ArrayList<ArrayList<String>> separateNodesAndEdges(ArrayList<String> file
     for(String line : fileLines){
 
         String[] parts = line.split(",");
-    
         int lengthline = parts.length;
-    
         if(lengthline ==3){
             edgeLines.add(line);
         }
@@ -33,46 +23,28 @@ public ArrayList<ArrayList<String>> separateNodesAndEdges(ArrayList<String> file
             nodeLines.add(line);
         }
     }
-
         ArrayList<ArrayList<String>> result = new ArrayList<>();
-
         result.add(edgeLines);
-
         result.add(nodeLines);
-
         return result;
         }
     
-
+//method2 for tree
 public ArrayList<String> readTreeInputFile(String path) {
     
-    //Create listOfInputLines(path)
-    String pathString = "/home/frank/java-opdrachten-frank/src/main/resources/intermediate/decision-tree-data.txt";
     ArrayList<String> fileLines = new ArrayList<String>();
-
     try {
-        Scanner scanner = new Scanner(new File(pathString));
-        
+        Scanner scanner = new Scanner(new File(path));
         while (scanner.hasNextLine()) {
-
             String line = scanner.nextLine();
-
-            //System.out.println(line);
-
             fileLines.add(line);
-
         }
-        
         scanner.close(); 
-
     } catch (FileNotFoundException e) {
         System.out.println("Bestand niet gevonden: " );
         e.printStackTrace();
     }
-
     return fileLines;
-
-    
 }
 
 
