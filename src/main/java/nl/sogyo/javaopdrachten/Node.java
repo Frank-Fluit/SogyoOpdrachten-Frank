f99e1febc55f8df4dc96b34f4282655106554677
Frank Fluit <ffluit@sogyo.nl>
2023-08-10 12:19:56 +0200

    instance variables enigzins aangepast

--- src/main/java/nl/sogyo/javaopdrachten/Node.java
+++ src/main/java/nl/sogyo/javaopdrachten/Node.java
@@ -1,27 +1,15 @@
 package nl.sogyo.javaopdrachten;
 
-import java.util.*;
-
 public class Node{
 
-
-    
-
-    
     Boolean question;
     String name;
     String questionOrAnswerLine;
-
-  
 
 public Node(Boolean question, String name, String questionorAnswerLine) {
     
     this.question = question;
     this.name = name;
-    this.questionOrAnswerLine = questionorAnswerLine;
-    
+    this.questionOrAnswerLine = questionorAnswerLine;    
 }
-
-
-
 }
