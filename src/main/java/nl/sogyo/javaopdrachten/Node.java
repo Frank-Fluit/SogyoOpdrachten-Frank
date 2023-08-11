a78418daca1826d80d2eab8cbac7311c10dae803
Frank Fluit <ffluit@sogyo.nl>
2023-08-11 11:11:42 +0200

    Moved relevant methods to this class

--- src/main/java/nl/sogyo/javaopdrachten/Node.java
+++ src/main/java/nl/sogyo/javaopdrachten/Node.java
@@ -1,5 +1,7 @@
 package nl.sogyo.javaopdrachten;
 
+import java.util.ArrayList;
+
 public class Node{
 
     Boolean question;
@@ -11,5 +13,60 @@ public Node(Boolean question, String name, String questionorAnswerLine) {
     this.question = question;
     this.name = name;
     this.questionOrAnswerLine = questionorAnswerLine;    
+}
+
+
+public static ArrayList<String> getDestinationNodes(ArrayList<Edge> edgesAsObjects){
+ArrayList<String> destinationNodes = new ArrayList<>();
+    for(Edge edge: edgesAsObjects){
+        String destinationNode = edge.destinationNode;
+        destinationNodes.add(destinationNode.trim());
+    }
+    return destinationNodes;
+}
+
+public static ArrayList<Node> nodesAsObjects(ArrayList<String> nodesStringList){
+
+
+    ArrayList<Node> nodesAsObjects = new ArrayList<>();
+    for(String line: nodesStringList){
+        String[] parts = line.split(",");
+        String nodename = parts[0].trim();
+        String questionOrAnswer = parts[1].trim();
+        char finalChar = questionOrAnswer.charAt(questionOrAnswer.length() - 1);
+        Boolean question = false;
+        if(Character.toString(finalChar).equals( "?")){
+            question = true;
+        }
+        else if(!Character.toString(finalChar).equals("?")){
+            question = false;
+        }
+        Node node = new Node(question,nodename,questionOrAnswer);
+        nodesAsObjects.add(node);
+    }
+
+
+    return nodesAsObjects;
+}
+
+public static Node findCurrentNode(ArrayList<Node> nodesAsObjects, ArrayList<String> destinationNodes){
+    
+    Node currentNode = null;
+    for (Node node : nodesAsObjects){
+        if(!destinationNodes.contains(node.name)){
+            currentNode = node;
+        }
+    }
+    return currentNode;
+}
+
+public static Node findNodeByName(String nodeName, ArrayList<Node> nodesAsObjects){
+    for (Node node : nodesAsObjects) {
+        if (nodeName.equals(node.name)) {
+            return node;
+        }
+    }
+    return null;
+
 }
 }
