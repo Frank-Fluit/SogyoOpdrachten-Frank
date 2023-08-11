021d685bd50fcb93cd50f915e5af41304b74a038
Frank Fluit <ffluit@sogyo.nl>
2023-08-11 11:10:40 +0200

    Moved relevant methods here

--- src/main/java/nl/sogyo/javaopdrachten/Tree.java
+++ src/main/java/nl/sogyo/javaopdrachten/Tree.java
@@ -6,8 +6,24 @@ import java.util.*;
 
 public class Tree{
 
-//method1 for tree
-public ArrayList<ArrayList<String>> separateNodesAndEdges(ArrayList<String> fileLines) { 
+public static ArrayList<String> readTreeInputFile(String path) {
+    
+    ArrayList<String> fileLines = new ArrayList<String>();
+    try {
+        Scanner scanner = new Scanner(new File(path));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            fileLines.add(line);
+        }
+        scanner.close(); 
+    } catch (FileNotFoundException e) {
+        System.out.println("Bestand niet gevonden: " );
+        e.printStackTrace();
+    }
+    return fileLines;
+}
+
+public static ArrayList<ArrayList<String>> separateNodesAndEdges(ArrayList<String> fileLines) { 
 
     ArrayList<String> nodeLines = new ArrayList<String>();
     ArrayList<String> edgeLines = new ArrayList<String>();
@@ -29,27 +45,46 @@ public ArrayList<ArrayList<String>> separateNodesAndEdges(ArrayList<String> file
         return result;
         }
     
-//method2 for tree
-public ArrayList<String> readTreeInputFile(String path) {
-    
-    ArrayList<String> fileLines = new ArrayList<String>();
-    try {
-        Scanner scanner = new Scanner(new File(path));
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            fileLines.add(line);
-        }
-        scanner.close(); 
-    } catch (FileNotFoundException e) {
-        System.out.println("Bestand niet gevonden: " );
-        e.printStackTrace();
-    }
-    return fileLines;
+
+public static String getUserInput() {
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextLine();
 }
 
+public static void runTree(Node currentNode,ArrayList<Node> nodesAsObjects,ArrayList<Edge> edgesAsObjects){
+Boolean endTreeReached = false;
+    while (!endTreeReached) {
+        System.out.println(currentNode.questionOrAnswerLine);
+        System.out.print("Beantwoord de vraag volgens het volgende format: {Ja}/{Nee}");
+        String inputUser = Tree.getUserInput();
+        if(inputUser.equals("ja")){
+            inputUser = "Ja";
+        }
+        if(inputUser.equals("nee")){
+            inputUser = "Nee";
+        }
+        System.out.println("Uw invoer: " + inputUser);
+        
+        Edge matchingEdge = Edge.findMatchingEdge(inputUser, currentNode.name, edgesAsObjects);
+        
+        if (matchingEdge != null) {
+            Node destinationNode = Node.findNodeByName(matchingEdge.destinationNode, nodesAsObjects);
+            
+            if (destinationNode != null) {
+                currentNode = destinationNode;
+                
+                if (!currentNode.question) {
+                    System.out.println("De boom waar u naar op zoek bent is: " + currentNode.questionOrAnswerLine);
+                    endTreeReached = true;
+                }
+            }
+        }
+    }
 
 
 
 
+
+}
 }
 
